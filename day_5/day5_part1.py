adc341e617f078bf4806b1cfbf71fccd71155d84
Brianna Frye <brianna.r.frye@gmail.com>
2020-12-05 15:19:38 -0600

    day 5

---
+++ day_5/day5_part1.py
@@ -0,0 +1,50 @@
+# AOC 2020, 
+# Day 5, Part 1
+
+# Objective: Examine list of boarding passes, find seat highest seat ID (row * 8 + column) in list. 
+# Boarding passes are in binary format
+# FBFBBFFRLR
+# F means go to first half, B means go to back half
+# Then L and R mean left and right half of the row (for determining column)
+# So, decoding FBFBBFFRLR reveals that it is the seat at row 44, column 5.
+
+# Basically will treat these like binary numbers-- because that's what they are.
+# And for this part, we just need to find the highest number.
+
+def parse_input(file_name):
+    data_left = True
+    input_array = []
+    try:
+        f = open(file_name, 'r')
+
+        while data_left == True:
+            temp = f.readline()
+            if temp == '':
+                data_left = False
+                break
+            input_array.append(temp)
+
+        return input_array
+    except Exception as e:
+        print(e)
+        return None
+
+def process_pass(pass_string):
+    # going to replace F and B with 0 and 1, and L and R with 0 and 1, respectively
+    # Then we'll have everything in binary
+    raw_binary = pass_string.replace('F', '0')
+    raw_binary = raw_binary.replace('B', '1')
+    raw_binary = raw_binary.replace('L', '0')
+    raw_binary = raw_binary.replace('R', '1')
+
+    return int(raw_binary, 2)
+
+passes = parse_input('day_5/input/day5_part1_input.txt')
+
+highest_pass = 0
+for item in passes:
+    pass_number = process_pass(item)
+    if pass_number > highest_pass:
+        highest_pass = pass_number
+
+print(highest_pass)
---
+++ day_5/day5_part2.py
@@ -0,0 +1,24 @@
+# AOC 2020, 
+# Day 5, Part 2
+
+# Objective: Find your seat number-- it will be the 'missing' seat number. However, some of the seats at the very front and back aren't on the flight, so that must be taken into account.
+
+# These are the methods from part 1 of today, will also just import them in the future
+from day5_part1 import parse_input, process_pass
+
+temp_passes = parse_input('day_5/input/day5_part1_input.txt')
+# print(temp_passes)
+passes = []
+
+lowest_num = 1500
+
+for item in temp_passes:
+    temp_pass_num = process_pass(item)
+    passes.append(temp_pass_num)
+    if temp_pass_num < lowest_num:
+        lowest_num = temp_pass_num
+
+for i in range(lowest_num, 1500):
+    if i not in passes:
+        print(i)
+        break
